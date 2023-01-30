1fad6767cd96627ccb03dcc74a53bd2cc7c94089
Kumar Prateek <kumarprateek902@gmail.com>
2023-01-30 14:09:43 +0530

    Stage-4

--- app/main.py
+++ app/main.py
@@ -1,28 +1,22 @@
 import subprocess
 import sys
+import os
+import shutil
+import tempfile
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
+        
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    returnCode = completed_process.returncode
-    output = completed_process.stdout.decode("utf-8")
-    error = completed_process.stderr.decode("utf-8")
-    if output:
-        output = output.strip("\n")
-        print(f"{output}")
+    with tempfile.TemporaryDirectory() as tempDir:
+        os.makedirs(os.path.join(tempDir, os.path.dirname(command).strip("/")))
+        shutil.copy(command, os.path.join(tempDir, command.strip("/")))
+        os.chroot(tempDir)
 
-    if error:
-        error = error.strip("\n")
-        print(f"{error}", file=sys.stderr)
-    return sys.exit(returnCode)
+        completed_process = subprocess.run([command, *args])
+        exit(completed_process.returncode)
 
 if __name__ == "__main__":
     main()
