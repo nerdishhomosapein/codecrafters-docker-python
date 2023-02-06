3483bb28819a9cc93d00ccb29e62856bdfc53baa
Kumar Prateek <kumarprateek902@gmail.com>
2023-02-06 18:08:13 +0530

    Stage-5

--- app/main.py
+++ app/main.py
@@ -3,13 +3,25 @@ import sys
 import os
 import shutil
 import tempfile
+import ctypes
+
 
 
 def main():
-        
+
     command = sys.argv[3]
     args = sys.argv[4:]
+    libc = ctypes.CDLL("libc.so.6", use_errno=True)
 
+    CLONE_NEWPID = 0x20000000
+
+    def unshare(flags):
+        ret = libc.unshare(flags)
+        if ret == -1:
+            err = ctypes.get_errno()
+            raise OSError(err, os.strerror(err))
+
+    unshare(CLONE_NEWPID)
     with tempfile.TemporaryDirectory() as tempDir:
         os.makedirs(os.path.join(tempDir, os.path.dirname(command).strip("/")))
         shutil.copy(command, os.path.join(tempDir, command.strip("/")))
@@ -17,6 +29,7 @@ def main():
 
         completed_process = subprocess.run([command, *args])
         exit(completed_process.returncode)
+
 
 if __name__ == "__main__":
     main()
